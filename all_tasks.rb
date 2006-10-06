6106f6ab5f29c6246b517f777ecac309a3dc4f70
Jan Berkel <jan.berkel@gmail.com>
2006-10-06 19:29:27 +0000

    all tasks

---
+++ all_tasks.rb
@@ -0,0 +1,10 @@
+$:.unshift File.join( File.dirname(__FILE__),  "lib" )
+
+require 'java_helper'
+require 'javac_task'
+require 'java_task'
+require 'javadoc_task'
+require 'testng_task'
+require 'jar_task'
+require 'apt_task'
+require 'jibx_task'
